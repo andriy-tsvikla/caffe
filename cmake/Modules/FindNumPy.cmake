34d1b06c64b7e813d7877c17835862919c787601
Andriy Tsvikla <7erikcartman@gmail.com>
2020-06-05 13:19:27 +0300

    message(STATUS "FindNumPy: Python executable ${PYTHON_EXECUTABLE}")

--- cmake/Modules/FindNumPy.cmake
+++ cmake/Modules/FindNumPy.cmake
@@ -16,6 +16,7 @@ unset(NUMPY_VERSION)
 unset(NUMPY_INCLUDE_DIR)
 
 if(PYTHONINTERP_FOUND)
+  message(STATUS "FindNumPy: Python executable ${PYTHON_EXECUTABLE}")
   execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
     "import numpy as n; print(n.__version__); print(n.get_include());"
     RESULT_VARIABLE __result
